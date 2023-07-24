bb043b9653d6e840f3a92f845f015e1563cd0434
raspberry <raspberryunlu@gmail.com>
2023-07-24 15:13:36 -0300

    detecting pressure & temperature

--- temperature-pressure/main.py
+++ temperature-pressure/main.py
@@ -5,21 +5,14 @@ import board
 import adafruit_bmp3xx
 
 # I2C setup
-i2c = board.I2C()  # uses board.SCL and board.SDA
-# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+i2c = board.I2C()
 bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
-
-# SPI setup
-# from digitalio import DigitalInOut, Direction
-# spi = board.SPI()
-# cs = DigitalInOut(board.D5)
-# bmp = adafruit_bmp3xx.BMP3XX_SPI(spi, cs)
 
 bmp.pressure_oversampling = 8
 bmp.temperature_oversampling = 2
 
 while True:
     print(
-        "Pressure: {:6.4f}  Temperature: {:5.2f}".format(bmp.pressure, bmp.temperature)
+        "Pressure: {:6.2f}  Temperature: {:5.2f}".format(bmp.pressure, bmp.temperature)
     )
     time.sleep(1)
