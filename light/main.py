53267379355ebc7593a99b31bf1205b41a570c58
raspberry <raspberryunlu@gmail.com>
2023-07-24 16:50:50 -0300

    detecting room light

--- light/main.py
+++ light/main.py
@@ -27,7 +27,7 @@ sensor = adafruit_tsl2591.TSL2591(i2c)
 while True:
 	# Read and calculate the light level in lux.
 	lux = sensor.lux
-	print("Total light: {0}lux".format(lux))
+	print("Total light: {0} lux".format(lux))
 	# You can also read the raw infrared and visible light levels.
 	# These are unsigned, the higher the number the more light of that type.
 	# There are no units like lux.
