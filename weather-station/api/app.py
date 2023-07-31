dbb511919310a0aac4660e5d7c0ed3d7a5fb2de7
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-07-31 10:10:10 -0300

    preparing the code for tests

--- weather-station/api/app.py
+++ weather-station/api/app.py
@@ -3,21 +3,30 @@ import threading
 import time
 from datetime import datetime
 
+# from src.IWeather import IWeather
+# from src.RaspberryStation import RaspberryStation
 from raspberry.humidity import getHumidity
 from raspberry.light import getLightLevels
 from raspberry.temperature_pressure import getTemperature, getPressure
 
 app = Flask(__name__)
 
-# Example data (temporary)
+# weatherStation: IWeather = RaspberryWeather()
+# data = {'light': weatherStation.getLightLevels(), 'pressure': weatherStation.getPressure(), 'humidity': weatherStation.getHumidity(), 'temperature': weatherStation.getTemperature(), 'time': datetime.now().isoformat()}
+
 data = {'light': getLightLevels(), 'pressure': getPressure(), 'humidity': getHumidity(), 'temperature': getTemperature(), 'time': datetime.now().isoformat()}
 
 # Function with sensor logic
 def collect_data_loop():
-    # Acces to example data variable (temporary)
     global data
+    # global weatherStation
+    # pressure, temperature, humidity, light = weatherStation.read_data()
     
     while True:
+        # data["temperature"] = weatherStation.getTemperature()
+        # data["humidity"] = weatherStation.getHumidity()
+        # data["pressure"] = weatherStation.getPressure()
+        # data["light"] = weatherStation.getLightLevels()
         data["temperature"] = getTemperature()
         data["humidity"] = getHumidity()
         data["pressure"] = getPressure()
